58e484a96cd855c641e8d50fb9519fa620a8909a
arifff42 <nuker_arif_bjk@hotmail.com>
2021-06-03 00:42:06 +0300

    03.06.2021

---
+++ src/app.js
@@ -0,0 +1,19 @@
+//var,let,const tipler
+let sayi1 = 10;
+sayi1 = "Arif YILDIZ";
+let student = { id: 1, name: "Arif" };
+//console.log(student);
+
+function save(puan = 10, ogrenci) {
+  //console.log(ogrenci.name + " : " + puan);
+}
+
+save(undefined, student);
+
+let students = ["Arif Yıldız", "Engin Demiroğ", "Büşra"];
+
+//console.log(students)
+
+let students2 = [student, { id: 2, name: "Arif" }, "Konya", { city: "Ankara" }];
+
+//console.log(students2)
